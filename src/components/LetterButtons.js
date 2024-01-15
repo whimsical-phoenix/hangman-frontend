0fb4d81961babe8985933451763c53f58bb901e8
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-14 19:04:31 -0700

    onClick logic

--- src/components/LetterButtons.js
+++ src/components/LetterButtons.js
@@ -1,15 +1,48 @@
+// // src/components/LetterButtons.js
+// import React from "react";
+
+// const LetterButtons = ({ onClick, disabledLetters }) => {
+//   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//   const handleButtonClick = (letter) => {
+//     console.log(`Button clicked: ${letter}`);
+//     onClick(letter);
+//   };
+
+//   return (
+//     <div className="letter-buttons">
+//       {alphabet.split("").map((letter, index) => (
+//         <button
+//           key={index}
+//           onClick={() => handleButtonClick(letter)}
+//           disabled={disabledLetters.includes(letter.toUpperCase())}>
+//           {letter}
+//         </button>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default LetterButtons;
+
+// // src/components/LetterButtons.js
 // src/components/LetterButtons.js
 import React from "react";
 
 const LetterButtons = ({ onClick, disabledLetters }) => {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  const handleButtonClick = (letter) => {
+    console.log(`Clicked letter: ${letter}`);
+    onClick(letter);
+  };
+
   return (
     <div className="letter-buttons">
       {alphabet.split("").map((letter, index) => (
         <button
           key={index}
-          onClick={() => onClick(letter)}
+          onClick={() => handleButtonClick(letter)}
           disabled={disabledLetters.includes(letter)}>
           {letter}
         </button>
