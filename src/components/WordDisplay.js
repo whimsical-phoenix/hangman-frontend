f3c32e97c4dab4e2a641830075717e4158120b57
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-14 19:04:52 -0700

    wordDisplay.js

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,3 +1,17 @@
+// // src/components/WordDisplay.js
+// import React from "react";
+
+// const WordDisplay = ({ word, guessedLetters }) => {
+//   const displayWord = word
+//     .split("")
+//     .map((letter, index) => (
+//       <span key={index}>{guessedLetters.includes(letter) ? letter : "_"}</span>
+//     ));
+
+//   return <div className="word-display">{displayWord}</div>;
+// };
+
+// export default WordDisplay;
 // src/components/WordDisplay.js
 import React from "react";
 
@@ -8,7 +22,12 @@ const WordDisplay = ({ word, guessedLetters }) => {
       <span key={index}>{guessedLetters.includes(letter) ? letter : "_"}</span>
     ));
 
-  return <div className="word-display">{displayWord}</div>;
+  return (
+    <div className="word-display">
+      <p>Number of Letters: {word.length}</p>
+      {displayWord}
+    </div>
+  );
 };
 
 export default WordDisplay;
