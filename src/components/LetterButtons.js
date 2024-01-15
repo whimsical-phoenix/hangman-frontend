89b2a9ddb3d83098a6dda806b791da2853f26ee1
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-14 19:23:57 -0700

    checking onClick functionality

--- src/components/LetterButtons.js
+++ src/components/LetterButtons.js
@@ -25,24 +25,18 @@
 
 // export default LetterButtons;
 
-// // src/components/LetterButtons.js
 // src/components/LetterButtons.js
 import React from "react";
 
 const LetterButtons = ({ onClick, disabledLetters }) => {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  const handleButtonClick = (letter) => {
-    console.log(`Clicked letter: ${letter}`);
-    onClick(letter);
-  };
-
   return (
     <div className="letter-buttons">
       {alphabet.split("").map((letter, index) => (
         <button
           key={index}
-          onClick={() => handleButtonClick(letter)}
+          onClick={() => onClick(letter)} // Make sure to use arrow function
           disabled={disabledLetters.includes(letter)}>
           {letter}
         </button>
