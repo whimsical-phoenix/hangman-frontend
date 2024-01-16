9700dadb97a1d484550717ef7212cae0e4f7e4d7
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-15 19:04:55 -0700

    added space btwn underscores, added spce between words logic

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,33 +1,41 @@
 // // src/components/WordDisplay.js
+
+import React from "react";
+
+const WordDisplay = ({ word, guessedLetters }) => {
+  return (
+    <div className="word-display">
+      {word.split("").map((letter, index) => (
+        <span
+          key={index}
+          className={
+            guessedLetters.includes(letter) || letter === " " ? "used" : ""
+          }>
+          {guessedLetters.includes(letter) || letter === " " ? letter : "_ "}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default WordDisplay;
+
+// src/components/WordDisplay.js
 // import React from "react";
 
 // const WordDisplay = ({ word, guessedLetters }) => {
 //   const displayWord = word
 //     .split("")
 //     .map((letter, index) => (
-//       <span key={index}>{guessedLetters.includes(letter) ? letter : "_"}</span>
+//       <span key={index}>{guessedLetters.includes(letter) ? letter : "_ "}</span>
 //     ));
 
-//   return <div className="word-display">{displayWord}</div>;
+//   return (
+//     <div className="word-display">
+//       <p>Number of Letters: {word.length}</p>
+//       {displayWord}
+//     </div>
+//   );
 // };
 
 // export default WordDisplay;
-// src/components/WordDisplay.js
-import React from "react";
-
-const WordDisplay = ({ word, guessedLetters }) => {
-  const displayWord = word
-    .split("")
-    .map((letter, index) => (
-      <span key={index}>{guessedLetters.includes(letter) ? letter : "_"}</span>
-    ));
-
-  return (
-    <div className="word-display">
-      <p>Number of Letters: {word.length}</p>
-      {displayWord}
-    </div>
-  );
-};
-
-export default WordDisplay;
