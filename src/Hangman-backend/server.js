1542a2a08ee2f21bcf14b31b67716c4a4a629ccb
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-22 12:53:28 -0700

    updating server.js file

--- src/Hangman-backend/server.js
+++ src/Hangman-backend/server.js
@@ -1,4 +1,37 @@
-require("dotenv").config();
+// //server.js
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const gameRouter = require("./routes/game");
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// // Enable CORS for all routes
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect to MongoDB (replace 'your_database_url' with your MongoDB connection string)
+// mongoose.connect(
+//   "mongodb+srv://admin:SZs5pWK3kF1M3Uxi@cluster0.t2xpdjz.mongodb.net/Gamesdb?retryWrites=true&w=majority"
+// );
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB connection established successful");
+// });
+
+// // Use API routes
+// app.use("/api/game", gameRouter);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// server.js
+
+require("dotenv").config(); // Load environment variables from .env file
 
 const express = require("express");
 const mongoose = require("mongoose");
