d54f2844ef1a11e95aecb4de2b3b82f7e96317f8
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-21 18:37:13 -0700

    removed sensitive info

--- src/Hangman-backend/server.js
+++ src/Hangman-backend/server.js
@@ -1,5 +1,3 @@
-//server.js
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,10 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB (replace 'your_database_url' with your MongoDB connection string)
-mongoose.connect(
-  "mongodb+srv://admin:SZs5pWK3kF1M3Uxi@cluster0.t2xpdjz.mongodb.net/Gamesdb?retryWrites=true&w=majority"
-);
+// Connect to MongoDB using environment variable directly
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
 connection.once("open", () => {
