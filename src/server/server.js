cc8db5a864878f3483b4d94015357f7744519ab4
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-26 11:39:20 -0700

    removed warnings from server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,36 +1,3 @@
-// //server.js
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const gameRouter = require("./routes/game");
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// // Enable CORS for all routes
-// app.use(cors());
-// app.use(express.json());
-
-// // Connect to MongoDB (replace 'your_database_url' with your MongoDB connection string)
-// mongoose.connect(
-//   "mongodb+srv://admin:SZs5pWK3kF1M3Uxi@cluster0.t2xpdjz.mongodb.net/Gamesdb?retryWrites=true&w=majority"
-// );
-
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("MongoDB connection established successful");
-// });
-
-// // Use API routes
-// app.use("/api/game", gameRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// server.js
-
 require("dotenv").config(); // Load environment variables from .env file
 
 const express = require("express");
@@ -45,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // optional: reduce the connection timeout
 });
 
 const connection = mongoose.connection;
