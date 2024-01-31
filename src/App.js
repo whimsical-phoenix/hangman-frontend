c27c1384736e699a32b57376083b7b8efd5d7769
whimsical-phoenix <shahprapti12@gmail.com>
2024-01-31 14:31:18 -0700

    reposition server folder

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/Game";
+import BackgroundCanvas from "./components/BackgroundCanvas";
 
 function App() {
   return (
     <div className="App">
+      <BackgroundCanvas />
       <Game />
     </div>
   );
